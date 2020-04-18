453579c8d27dddd978cf44f3e34835561d1fad4c
BuTR3b <43819843+BuTR3b@users.noreply.github.com>
2020-04-18 17:56:15 +0300

    Add files via upload

---
+++ Notebook/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Notebook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            StreamWriter sw = new StreamWriter(path: @"C:\test\test.txt", append: true);
+            sw.Write(textBox1.Text);
+            sw.Close();
+            */
+            File.WriteAllText(path: @"C:\test\test.txt", contents: textBox1.Text);
+            File.AppendAllText(path: @"C:\test\test.txt", contents: textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Process.Start("notepad.exe", @"C:\test\test.txt");
+            /*
+            StreamReader sr = new StreamReader(path: @"C:\test\test.txt");
+            textBox1.Text = sr.ReadToEnd();
+            sr.Close();
+            */
+            string s = File.ReadAllText(path: @"C:\test\test.txt");
+            MessageBox.Show(s);
+        }
+    }
+}
