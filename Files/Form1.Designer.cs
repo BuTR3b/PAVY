c4dbf81cb4ae064845fe47f19981a7ff8879dd70
BuTR3b <43819843+BuTR3b@users.noreply.github.com>
2020-04-18 18:00:23 +0300

    Add files via upload

---
+++ Files/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.save_button = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // open_button
+            // 
+            this.open_button.Location = new System.Drawing.Point(12, 32);
+            this.open_button.Name = "open_button";
+            this.open_button.Size = new System.Drawing.Size(75, 23);
+            this.open_button.TabIndex = 0;
+            this.open_button.Text = "Open";
+            this.open_button.UseVisualStyleBackColor = true;
+            this.open_button.Click += new System.EventHandler(this.open_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(38, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N";
+            // 
+            // save_button
+            // 
+            this.save_button.Location = new System.Drawing.Point(12, 61);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(75, 23);
+            this.save_button.TabIndex = 3;
+            this.save_button.Text = "Save";
+            this.save_button.UseVisualStyleBackColor = true;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 79);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 96);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.open_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Files/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.ScrollBars = ScrollBars.Vertical;
+        }
+
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            int n;
+            try
+            {
+                n = Convert.ToInt32(textBox1.Text);
+                StreamWriter file = new StreamWriter(@"C:\test\test.txt");
+                for (int i = 2; i <= n; i++)
+                {
+                    bool prime = true;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0 & i % 1 == 0)
+                        {
+                            prime = false;
+                        }
+                    }
+                    if (prime)
+                    {
+                        textBox2.Text += i + "\r\n";
+                        file.WriteLine(Convert.ToString(i));
+                    }
+                    else
+                    {
+                        textBox2.Text += i + " ";
+                        file.Write(Convert.ToString(i) + " ");
+                    }
+                }
+                file.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Введите N");
+            }
+        }
+
+        private void open_button_Click(object sender, EventArgs e)
+        {
+            StreamReader file = new StreamReader(@"C:\test\test.txt");
+            textBox2.Text = file.ReadToEnd();
+            file.Close();
+        }
+    }
+}
