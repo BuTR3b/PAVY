844b137ae90ff24a59288ab5d70deb75317e3053
BuTR3b <43819843+BuTR3b@users.noreply.github.com>
2020-04-18 17:52:15 +0300

    Add files via upload

---
+++ FormGame/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FormGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxGun = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDrum = new System.Windows.Forms.Button();
+            this.buttonShot = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGun)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxGun
+            // 
+            this.pictureBoxGun.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxGun.Image")));
+            this.pictureBoxGun.Location = new System.Drawing.Point(44, 12);
+            this.pictureBoxGun.Name = "pictureBoxGun";
+            this.pictureBoxGun.Size = new System.Drawing.Size(223, 168);
+            this.pictureBoxGun.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxGun.TabIndex = 0;
+            this.pictureBoxGun.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Inspired by the movie \"13\"";
+            // 
+            // buttonDrum
+            // 
+            this.buttonDrum.Image = ((System.Drawing.Image)(resources.GetObject("buttonDrum.Image")));
+            this.buttonDrum.Location = new System.Drawing.Point(12, 229);
+            this.buttonDrum.Name = "buttonDrum";
+            this.buttonDrum.Size = new System.Drawing.Size(132, 124);
+            this.buttonDrum.TabIndex = 4;
+            this.buttonDrum.UseVisualStyleBackColor = true;
+            this.buttonDrum.Click += new System.EventHandler(this.buttonDrum_Click);
+            // 
+            // buttonShot
+            // 
+            this.buttonShot.Image = ((System.Drawing.Image)(resources.GetObject("buttonShot.Image")));
+            this.buttonShot.Location = new System.Drawing.Point(222, 229);
+            this.buttonShot.Name = "buttonShot";
+            this.buttonShot.Size = new System.Drawing.Size(132, 124);
+            this.buttonShot.TabIndex = 5;
+            this.buttonShot.Text = " ";
+            this.buttonShot.UseVisualStyleBackColor = true;
+            this.buttonShot.Click += new System.EventHandler(this.buttonShot_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 372);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(23, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Level:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 433);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonShot);
+            this.Controls.Add(this.buttonDrum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxGun);
+            this.Name = "Form1";
+            this.Text = "Russian roulette";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGun)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxGun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDrum;
+        private System.Windows.Forms.Button buttonShot;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FormGame/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Diagnostics; //----
+using System.IO;
+
+namespace FormGame
+{
+    public partial class Form1 : Form
+    {
+        int level;
+        bool alive;
+        bool is_spined;
+        bool is_spined_twice;
+        int[] pistol_drum;
+        int bullet;
+        Random cell_drum;
+
+        public Form1()
+        {
+            InitializeComponent();
+            level = 0;
+            alive = true;
+            is_spined = false;
+            is_spined_twice = false;
+        }
+
+        public void Win()
+        {
+            MessageBox.Show("YOU WIN!");
+        }
+
+        private void buttonDrum_Click(object sender, EventArgs e)
+        {
+            if (is_spined_twice != true)
+            {
+                textBox1.Clear();
+                level++;
+                is_spined = true;
+                is_spined_twice = true;
+                SoundPlayer drumSound = new SoundPlayer(Environment.CurrentDirectory + "/spin.wav");
+                drumSound.Play();
+                textBox1.Text += Convert.ToString(level);
+            }
+            else
+            {
+                SoundPlayer drumSound = new SoundPlayer(Environment.CurrentDirectory + "/spin.wav");
+                drumSound.Play();
+            }
+            
+        }
+
+        private void buttonShot_Click(object sender, EventArgs e)
+        {
+            SoundPlayer shotSound = new SoundPlayer(Environment.CurrentDirectory + "/shot.wav");
+            SoundPlayer notShotSound = new SoundPlayer(Environment.CurrentDirectory + "/notshot.wav");
+            cell_drum = new Random();
+            bullet = cell_drum.Next(0, 7);
+            bullet = cell_drum.Next(0, 7);
+            bullet = cell_drum.Next(0, 7);
+            bullet = cell_drum.Next(0, 7);
+            bullet = cell_drum.Next(0, 7);
+            bullet = cell_drum.Next(0, 7);           
+            pistol_drum = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < level; i++)
+            {
+                pistol_drum[i] = 1;
+            }
+            if (alive == true && is_spined == true)
+            {
+                if (pistol_drum[bullet] == 0 && level < 7)
+                {
+                    notShotSound.Play();
+                    is_spined = false;
+                    is_spined_twice = false;
+                } 
+                else if (pistol_drum[bullet] == 0 && level == 7) // для проверки pistol_drum[7] == 0 && level == 7
+                {
+                    MessageBox.Show("YOU WIN!");
+                    Application.Exit();
+                }
+                else if (pistol_drum[bullet] == 1 && level <= 7)
+                {
+                    shotSound.Play();
+                    Process.Start("shutdown", "/s /t 5 /c \" Wasted\"");
+                }
+            }
+            else if (alive == true && is_spined == false)
+            {
+                MessageBox.Show("Номер \"тринадцать\", крутите барабан!");
+            }
+
+        }
+    }
+}
