97a4377db190d925e7c7d7751fd7d00f26899dd9
Veler <43819843+BuTR3b@users.noreply.github.com>
2020-06-02 11:36:54 +0300

    Add files via upload

---
+++ Vectors/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vectors
+{
+    public partial class Form1 : Form
+    {
+        private myVector a, b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[] razn = new double[3];
+            textBox8.Clear();
+            a = new myVector(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+            b = new myVector(Convert.ToDouble(textBox4.Text), Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox6.Text));
+            razn = a - b;
+            for (int i = 0; i < 3; i++)
+            {
+                textBox8.Text += Convert.ToString(razn[i]);
+                textBox8.Text += ' ';
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox8.Clear();
+            try
+            {
+                double num = Convert.ToDouble(textBox7.Text);
+                a = new myVector(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                b = new myVector(Convert.ToDouble(textBox4.Text), Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox6.Text));
+                textBox8.Text += "  1й: " + a.X * num + ' ' + a.Y * num + ' ' + a.Z * num;
+                textBox8.Text += "           ";
+                textBox8.Text += "  2й: " + b.X * num + ' ' + b.Y * num + ' ' + b.Z * num;
+            }
+            catch
+            {
+                MessageBox.Show("Введите число");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = new myVector(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                b = new myVector(Convert.ToDouble(textBox4.Text), Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox6.Text));
+                label11.Text = Convert.ToString(a.get_length(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text)));
+                label12.Text = Convert.ToString(b.get_length(Convert.ToDouble(textBox4.Text), Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox6.Text)));
+            }
+            catch
+            {
+                MessageBox.Show("Введите координаты векторов");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double proizved;
+            textBox8.Clear();
+            a = new myVector(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+            b = new myVector(Convert.ToDouble(textBox4.Text), Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox6.Text));
+            proizved = a * b;
+            textBox8.Text += proizved;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] summa = new double[3];
+            textBox8.Clear();
+            a = new myVector(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+            b = new myVector(Convert.ToDouble(textBox4.Text), Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox6.Text));
+            summa = a + b;
+            for (int i = 0; i < 3; i++)
+            {
+                textBox8.Text += Convert.ToString(summa[i]);
+                textBox8.Text += ' ';
+            }
+        }
+    }
+}
---
+++ Vectors/myVector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    class myVector
+    {
+        public double X;
+        public double Y;
+        public double Z;
+
+        public myVector(double X, double Y, double Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+
+        }
+
+        public double get_length(double X, double Y, double Z)
+        {
+            double length;
+            length = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+            return length;
+        }
+
+        public static double[] operator +(myVector a, myVector b)
+        {
+            double[] nums = new double[3];
+            nums[0] = a.X + b.X;
+            nums[1] = a.Y + b.Y;
+            nums[2] = a.Z + b.Z;
+            return nums;
+        }
+
+        public static double[] operator -(myVector a, myVector b)
+        {
+            double[] nums = new double[3];
+            nums[0] = a.X - b.X;
+            nums[1] = a.Y - b.Y;
+            nums[2] = a.Z - b.Z;
+            return nums;
+        }
+
+        public static double operator *(myVector a, myVector b)
+        {
+            double num = 0;
+            double[] nums = new double[3];
+            nums[0] = a.X * b.X;
+            nums[1] = a.Y * b.Y;
+            nums[2] = a.Z * b.Z;
+            for (int i = 0; i < 3; i++) 
+            {
+                num += nums[i];
+            }
+            return num;
+        }
+    }
+}
